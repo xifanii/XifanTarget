f69ef0cd16172555fae2c0d6d68ee99c2cd918fc
xifanii <xifanii@foxmail.com>
2017-03-17 00:55:28 +0800

    初始化项目

---
+++ XifanTarget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XifanTarget
+//
+//  Created by zhu on 2017/3/17.
+//  Copyright © 2017年 zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XifanTarget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XifanTarget
+//
+//  Created by zhu on 2017/3/17.
+//  Copyright © 2017年 zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
